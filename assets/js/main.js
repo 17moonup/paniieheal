780c8223f3ce0368b2966ca3eac60d5cf93c589e
17moonup <17moonup@gmail.com>
2023-12-05 18:43:05 +0800

    1205

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,24 @@
-function flipBox() {
-  var box1 = document.getElementsByClassName('box')[0];
-  var box2 = document.getElementsByClassName('box')[1];
+/*
+ * 1.click to trigger the anima.(js)
+ * 2.fill & filp to the details side.(css + js)
+ * 3.click again to initial.(js)
+*/
 
-  box1.classList.toggle('flipped');
-  box2.classList.toggle('flipped');
-}
+/* rotate-photo*/
+let imgcontainer = document.getElementById("rotate-me");
+let imgElement = imgcontainer.querySelector('img');
+
+imgElement.onclick = function() {
+	let mySrc = imgElement.getAttribute("src");
+	if (mySrc === "img/5t5.jpg") {
+		imgElement.setAttribute("src", "img/kkx.png");
+	} else {
+		imgElement.setAttribute("src", "img/5t5.jpg");
+	}
+};
+ 
+const image = document.getElementById('image');
+
+image.addEventListener('click', function() {
+ image.style.animationPlayState = 'running';
+  });
