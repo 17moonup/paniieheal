38c16b2655900e558ef5df3d0448ea0e80480674
17moonup <17moonup@gmail.com>
2023-12-06 17:47:52 +0800

    1206

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,6 @@
-/*
- * 1.click to trigger the anima.(js)
- * 2.fill & filp to the details side.(css + js)
- * 3.click again to initial.(js)
-*/
-
 /* rotate-photo*/
-let imgcontainer = document.getElementById("rotate-me");
-let imgElement = imgcontainer.querySelector('img');
+let imgContainer = document.getElementById("rotate-me");
+let imgElement = imgContainer.querySelector('img');
 
 imgElement.onclick = function() {
 	let mySrc = imgElement.getAttribute("src");
@@ -16,9 +10,20 @@ imgElement.onclick = function() {
 		imgElement.setAttribute("src", "img/5t5.jpg");
 	}
 };
- 
-const image = document.getElementById('image');
 
-image.addEventListener('click', function() {
- image.style.animationPlayState = 'running';
-  });
+/*
+ * 1.click to trigger the anima.(js)
+ * 2.fill & filp to the details side.(css + js)
+ * 3.click again to initial.(js)
+*/
+
+const fillflip = document.querySelector('.boxes');
+
+fillflip.addEventListener('click', function(event) {
+	const target = event.target;
+	if (target.classList.contains('image')) {
+		target.classList.toggle('enlarged');
+		target.closest('.boxpreview').classList.toggle('flipped');
+	}
+});
+
