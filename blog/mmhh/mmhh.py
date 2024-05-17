5e6f83f8fd356a85b9f60ea57e9c5d1cc87b26c4
17moonup <17moonup@gmail.com>
2024-05-17 23:47:19 +0800

    mmhh

---
+++ blog/mmhh/mmhh.py
@@ -0,0 +1,31 @@
+import os
+import markdown
+import argparse
+from jinja2 import Environment, FileSystemLoader
+
+parser = argparse.ArgumentParser(description="test")
+parser.add_argument('input', type=str, help='Input file')
+
+args = parser.parse_args()
+
+filename = args.input
+
+source_dir = '../books/'
+output_dir = '../../post/books/'
+template_dir = 'templates'
+
+env = Environment(loader=FileSystemLoader(template_dir))
+
+with open(os.path.join(source_dir, filename), 'r') as file:
+    markdown_content = file.read()
+
+html_content = markdown.markdown(markdown_content)
+
+template = env.get_template('books.html')
+
+output_html = template.render(content=html_content)
+
+output_path = os.path.join(output_dir, os.path.splitext(filename)[0] + '.html')
+
+with open(output_path, 'w') as file:
+    file.write(output_html)
